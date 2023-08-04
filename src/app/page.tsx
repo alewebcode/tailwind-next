f44f0419f0cf690d708b148bedabe9893d552f2d
alexandreinspell <alexandre@inspell.com.br>
2023-08-04 16:13:12 -0300

    adicionando seletores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className="text-cyan-300 font-semibold">Hello</h1>
+    <div className="p-8">
+    <h1 className="font-bold text-5xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">Hello Tailwind</h1>
+    <button disabled className="bg-sky-400 px-4 py-2 rounded-md font-medium text-slate-50 mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
+      Sign in
+    </button>
+
+    </div>
   )
 }
